e7f5c290c5d6fd58b69eb8b0af17fe68b2de03f5
lordking32 <ibrahimadia806@gmail.com>
2025-05-27 08:11:03 +0000

    Changement pour teacherService

---
+++ teacherService/src/main/java/sn/edu/ugb/teacher/feign/CoursClient.java
@@ -0,0 +1,13 @@
+package sn.edu.ugb.teacher.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import sn.edu.ugb.teacher.feign.CoursDTO;
+
+@FeignClient(name = "cursusService")
+public interface CoursClient {
+
+    @GetMapping("/api/cours/{id}")
+    CoursDTO getCoursById(@PathVariable("id") Long id);
+}
